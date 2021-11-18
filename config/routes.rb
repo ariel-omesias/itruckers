aee5ee20f7c42b7217aa2f2a5fc00f84d1e5995a
Ariel Orellana <a.orellana.mesias@gmail.com>
2021-11-18 12:13:00 -0300

    add navbar

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :appointments
   resources :trucks
   resources :users
+  root to: 'trucks#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
