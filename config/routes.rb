b4074c8f965fbe59f5290b9c3001a6f8e6ebbf6d
Ariel Orellana <a.orellana.mesias@gmail.com>
2021-11-22 12:20:58 -0300

    install devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :appointments
   resources :trucks
-  resources :users
+  #resources :users
   root to: 'trucks#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
